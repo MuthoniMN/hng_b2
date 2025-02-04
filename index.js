f91be4abb2026eeb6b713cb86c2e7c9e8d253349
Mitch <ndianguimichelle@gmail.com>
2025-02-04 09:38:26 +0300

    feat: functional for positive numbers

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+
+const getProperties = (number) => {
+  const props = [];
+  let sum = 0
+
+  if (number % 2 === 0){
+    props.push('even');
+  }else {
+    props.push('odd')
+  }
+
+  const splitNumbers = number.toString().split('').map(n => Number(n));
+
+  splitNumbers.forEach(element => {
+    const cube = element **3;
+
+    sum += cube
+  });
+
+  if(sum === number){
+    props.push('armstrong')
+  }
+
+  return props;
+}
+
+const isPerfect = (number) => {
+  let sum = 1;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+          sum += i;
+          if (i !== number / i) {
+              sum += number / i;
+          }
+      }
+  }
+
+  return sum === number;
+}
+
+const isPrime = (number) => {
+  let prime = true;
+
+  for (let i = 2; i <= number/2; i++) {
+    if (number % i == 0) {
+        prime = false;
+        break;
+    }
+  }
+
+  return prime;
+}
+
+const digitSum = (number) => {
+  const sum = Math.absolute(number).toString().split('').map(n => Number(n)).reduce((acc, curr) => acc + curr, 0);
+
+  return sum;
+}
+
+app.get('/api/classify-number', async (req, res) => {
+  const { number } = req.query;
+
+  if(!Number(number)){
+    return res.json({
+      'number': 'alphabet',
+      'error': true
+    }).status(400);
+  }
+
+  const response = await fetch(`http://numbersapi.com/${number}?json`);
+  const fact = await response.json();
+  console.log(getProperties(Number(number)));
+
+  return res.json({
+    'number': Number(number),
+    'is_prime': isPrime(Number(number)),
+    'is_perfect': isPerfect(Number(number)),
+    'properties': getProperties(Number(number)),
+    'fun_fact': fact.text,
+    'digits_sum': digitSum(Number(number))
+  }).status(200);
+});
+
+app.listen(5000, () => console.log("Server is running!"));
